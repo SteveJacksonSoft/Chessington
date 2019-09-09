0ad9f9f0d34fc6fa07c9f73664bc3047436bbdd3
Steve <stephen.jackson@softwire.com>
2019-09-09 14:05:01 +0100

    Pawns can move 1 square except when on starting rank ;

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -11,10 +11,21 @@ namespace Chessington.GameEngine.Pieces
         public override IEnumerable<Square> GetAvailableMoves(Board board) {
             Square currentPosition = board.FindPiece(this);
 
-            return new List<Square> {
-                new Square(currentPosition.Row + 1, currentPosition.Col),
-                new Square(currentPosition.Row - 1, currentPosition.Col)
-            };
+            List<Square> availableMoves = new List<Square>();
+
+            if (Player == Player.White) {
+                availableMoves.Add(Square.At(currentPosition.Row - 1, currentPosition.Col));
+                if (currentPosition.Row == 6) {
+                    availableMoves.Add(Square.At(currentPosition.Row - 2, currentPosition.Col));
+                }
+            } else {
+                availableMoves.Add(Square.At(currentPosition.Row + 1, currentPosition.Col));
+                if (currentPosition.Row == 1) {
+                    availableMoves.Add(Square.At(currentPosition.Row + 2, currentPosition.Col));
+                }
+            }
+
+            return availableMoves;
         }
     }
 }
